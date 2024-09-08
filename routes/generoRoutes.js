63ae47b335404ef6351c9facf332e6c5770a35eb
Daca.dev <daca.dev97@gmail.com>
2024-09-08 18:29:37 -0500

    Refactor generoRoutes.js to adhere to coding style guidelines

--- routes/generoRoutes.js
+++ routes/generoRoutes.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const { crearGenero, editarGenero, eliminarGenero, obtenerGeneros } = require('../controllers/generoController');
+const express = require("express");
+const {
+  crearGenero,
+  editarGenero,
+  eliminarGenero,
+  obtenerGeneros,
+} = require("../controllers/generoController");
 const router = express.Router();
 
-router.get('/', obtenerGeneros);
-router.post('/crearGenero', crearGenero);
-router.put('/:id', editarGenero);
-router.delete('/:id', eliminarGenero);
+router.get("/", obtenerGeneros);
+router.post("/", crearGenero);
+router.put("/:id", editarGenero);
+router.delete("/:id", eliminarGenero);
 
 module.exports = router;
-
