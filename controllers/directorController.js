d19bca00524ecc75f01831bfdc77a1f614e81f73
Daca.dev <daca.dev97@gmail.com>
2024-09-08 18:44:37 -0500

    Refactor database configuration file and genero controller and routes

---
+++ controllers/directorController.js
@@ -0,0 +1,58 @@
+const Genero = require("../models/director");
+
+const crearDirector = async (req, res) => {
+  try {
+    console.log("Datos recibidos:", req.body);
+    const director = await Director.create(req.body);
+    res.status(201).json(director);
+  } catch (error) {
+    console.error("Error al crear director:", error);
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const editarDirector = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const director = await Director.findByPk(id);
+
+    if (!director) {
+      return res.status(404).json({ error: "Director no encontrado" });
+    }
+    await director.update(req.body);
+    res.json(director);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const eliminarDirector = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const director = await Director.findByPk(id);
+
+    if (!director) {
+      return res.status(404).json({ error: "Director no encontrado" });
+    }
+    await director.destroy();
+    res.json({ message: "Director eliminado" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const obtenerDirectores = async (req, res) => {
+  try {
+    const director = await Director.findAll();
+    res.json(director);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  crearDirector,
+  editarDirector,
+  eliminarDirector,
+  obtenerDirectores,
+};
--- models/director.js
+++ models/director.js
@@ -1,26 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Director extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Director.init({
-    nombres: DataTypes.STRING,
-    estado: DataTypes.BOOLEAN,
-    fecha_creacion: DataTypes.DATE,
-    fecha_actualizacion: DataTypes.DATE
-  }, {
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Genero = sequelize.define(
+  "Director",
+  {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    fecha_creacion: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    fecha_actualizacion: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
     sequelize,
-    modelName: 'Director',
-  });
-  return Director;
-};
+    modelName: "Director",
+  }
+);
+
+module.exports = Director;
